b40b21fea6f01a7425f70dc43a9d603dedf78072
Juan F Carlos <juanfcarlos.93@gmail.com>
2018-06-06 14:54:22 -0300

    Inserindo Primeiras Funcionalidades Funcionalidades de Pouso e Decolagem

--- Aeroporto/src/aeroporto/Aeroporto.java
+++ Aeroporto/src/aeroporto/Aeroporto.java
@@ -15,8 +15,22 @@ public class Aeroporto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         
+        //Primeiro instanciamos uma pista
+        Pista pista = new Pista();
+        
+        Aeronave a1 = new Aeronave("VOO 1", pista);
+        Aeronave a2 = new Aeronave("VOO 2", pista);
+        Aeronave a3 = new Aeronave("VOO 3", pista);
+        Aeronave a4 = new Aeronave("VOO 4", pista);
+        Aeronave a5 = new Aeronave("VOO 5", pista);
+		
+        pista.start();
+        a1.start();
+        a2.start();
+        a3.start();
+        a4.start();
+        a5.start();
     }
     
 }
